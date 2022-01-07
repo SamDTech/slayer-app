e12a666c52e680da9d5d567b40a629d65592a1f0
sam <opeyemisamuel222@gmail.com>
2022-01-07 16:46:23 +0100

    get the user input and clean it

---
+++ interaction/input.go
@@ -0,0 +1,28 @@
+package interaction
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func GetPlayerChoice(specialAttackIsAvailable bool){
+
+}
+
+func getPlayerInput() (string, error){
+	fmt.Print("Your Choice:  ")
+
+	userInput, err := reader.ReadString('\n')
+
+	if err != nil{
+		return "", err
+	}
+	userInput = strings.Replace(userInput, "\n", "", -1)
+
+	return userInput, nil
+
+}
