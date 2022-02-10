66e0b8510d062bcd25588f28d53698827105df99
sam <opeyemisamuel222@gmail.com>
2022-02-10 07:13:38 +0100

    get the current player health and monster

--- actions/actions.go
+++ actions/actions.go
@@ -14,7 +14,9 @@ var currentPlayerHealth = PLAYER_HEALTH
 // generate random number
 var randGenerator = rand.New(randSource)
 
-func AttackMoster(isSpecialAttack bool) {
+
+// function to Attack Monster
+func AttackMoster(isSpecialAttack bool)  {
 	minAttackValue := PLAYER_ATTACK_MIN_DMG
 	maxAttackValue := PLAYER_ATTACK_MAX_DMG
 
@@ -26,9 +28,10 @@ func AttackMoster(isSpecialAttack bool) {
 	dmgValue := generateRandBetween(minAttackValue, maxAttackValue)
 	// subtract monster health from the damage value
 	currentMonsterHealth -= dmgValue
+
 }
 
-func HealPlayer() {
+func HealPlayer()  {
 
 	healValue := generateRandBetween(PLAYER_HEAL_MIN_VALUE, PLAYER_HEAL_MAX_VALUE)
 
@@ -42,9 +45,11 @@ func HealPlayer() {
 
 	// add player health from the damage value
 	currentPlayerHealth += healValue
+
+
 }
 
-func AttackPlayer() {
+func AttackPlayer()  {
 	minAttackValue := MONSTER_ATTACK_MIN_DMG
 	maxAttackValue := MONSTER_ATTACK_MAX_DMG
 
@@ -52,8 +57,15 @@ func AttackPlayer() {
 	dmgValue := generateRandBetween(minAttackValue, maxAttackValue)
 	// subtract monster health from the damage value
 	currentPlayerHealth -= dmgValue
+
+
 }
 
+
+// function to get current player and monster health
+func GetCurrentPlayerHealth() (int, int) {
+	return currentPlayerHealth, currentMonsterHealth
+}
 
 // function to generate random number between min and max
 func generateRandBetween(min int, max int) int {
--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/samdtech/slayer-app/actions"
 	"github.com/samdtech/slayer-app/interaction"
 )
 
@@ -26,20 +25,36 @@ func startGame() {
 
 func executeRounds() string {
 	currentRound++
-	isSpecialRound := currentRound % 3 == 0
-
-	fmt.Print(isSpecialRound)
+	isSpecialRound := currentRound%3 == 0
 
 	interaction.ShowAvailableActions(isSpecialRound)
 
 	userChoice := interaction.GetPlayerChoice(isSpecialRound)
 
+	var playerHeath int
+	var monsterHealth int
+
 	if userChoice == "ATTACK" {
+		actions.AttackMoster(false)
 
-	}else if userChoice == "HEAL"{
-
+	} else if userChoice == "HEAL" {
+		 actions.HealPlayer()
 	} else {
+		// now we are in special round
+		 actions.AttackMoster(true)
+	}
 
+	// monster should always attack
+	 actions.AttackPlayer()
+
+	// get the current health
+	playerHeath, monsterHealth = actions.GetCurrentPlayerHealth()
+
+	// check if player or monster is dead
+	if playerHeath <= 0 {
+		return "Monster"
+	} else if monsterHealth <= 0{
+		return "Player"
 	}
 
 	return ""
